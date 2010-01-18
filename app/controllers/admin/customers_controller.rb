9ed0fb8256e9f0c4ff68e758e3d9d064e0260b70
jhass <jhass@jhass-laptop.(none)>
2010-01-18 14:12:47 -0400

    cliente en administrador realizado

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,6 +3,21 @@ class Admin::CustomersController < ApplicationController
    layout 'admin'   
  
    def index
-   
+    
+    @buyers = Buyer.paginate(
+      :order => "name ASC",
+      :page => params[:page],
+      :per_page => 30
+    )
    end
+
+   def edit
+       @buyer = Buyer.find(params[:id])
+   end
+   
+   def destroy 
+       Buyer.find(params[:id]).destroy
+       redirect_to :action => 'index'
+   end
+
 end
